c804985c259439df9fca718ce1c114b3cc8cd6c7
zhendozzz <info@zhendozzz.ru>
2021-03-15 17:37:40 +0300

    init: initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        const element = bracketsConfig[i];
+        const elementForSearch = element[0] + element[1];
+        while (str.indexOf(elementForSearch) != -1) {
+            str = str.replace(elementForSearch, '');
+            i = -1;
+        }
+    }
+    return str.length == 0;
 }
